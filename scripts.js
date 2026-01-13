2008ee25917171c259b6e64064e57279851ac3e7
Tcon84 <tiagocondeco@gmail.com>
2026-01-13 13:52:04 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const ano = document.getElementById("ano-atual");
-    if (ano) {
-        ano.textContent = new Date().getFullYear();
-    }
+
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML("beforeend", data);
+
+                       const ano = document.getElementById("ano-atual");
+            if (ano) {
+                ano.textContent = new Date().getFullYear();
+            }
+        })
+        .catch(error => {
+            console.error("Erro ao carregar o footer:", error);
+        });
+
 });
