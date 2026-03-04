439ee623e0da1da8e50b578a9b08fe697f72f47b
Tcon84 <tiagocondeco@gmail.com>
2026-03-04 13:04:18 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,3 +15,59 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
 });
+function searchSite() {
+
+    let input = document.getElementById("siteSearch");
+    let filter = input.value.toLowerCase();
+
+    removeHighlights();
+
+    if (filter.length < 2) return;
+
+    let content = document.querySelector("section");
+
+    highlightText(content, filter);
+}
+
+function highlightText(element, keyword) {
+
+    if (!element) return;
+
+    let walker = document.createTreeWalker(
+        element,
+        NodeFilter.SHOW_TEXT,
+        null,
+        false
+    );
+
+    let node;
+    while(node = walker.nextNode()) {
+
+        if(node.nodeValue.toLowerCase().includes(keyword)) {
+
+            let span = document.createElement("span");
+            span.className = "highlight";
+
+            let regex = new RegExp(`(${keyword})`, "gi");
+
+            span.innerHTML =
+                node.nodeValue.replace(regex, "<mark>$1</mark>");
+
+            node.parentNode.replaceChild(span, node);
+        }
+    }
+
+    let first = document.querySelector("mark");
+    if(first){
+        first.scrollIntoView({
+            behavior: "smooth",
+            block: "center"
+        });
+    }
+}
+
+function removeHighlights() {
+    document.querySelectorAll("mark").forEach(el => {
+        el.replaceWith(el.textContent);
+    });
+}
